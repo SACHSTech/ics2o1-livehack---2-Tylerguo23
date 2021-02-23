e3a5b4d009c50bdad0f9fa6b30f49b2f97eb3e78
Tyler Guo <tyler.guo23@ycdsbk12.ca>
2021-02-23 15:12:30 +0000

    Final submission

--- problem1.py
+++ problem1.py
@@ -1,1 +1,38 @@
+"""
+----------------------------------------------------------------------
+Name:      problem1.py
 
+Purpose:   This program will determine the type of martian being detected using the number of eyes and antenna being inputted
+ 
+Author:    Guo.T
+ 
+Created:   23/02/2021
+----------------------------------------------------------------------
+"""
+print("******* Martian Classifier ********")
+print()
+
+#take user input for the number of eyes and antennas
+antennas = int(input("How many antennas are detected?: "))
+eyes = int(input("How many eyes are detected?: "))
+
+#boolean to track whether at least one of the conditions are met
+found_alien = False
+
+#check each condition and output 
+if antennas >= 3 and eyes <= 4:
+  print("Life form detected: AudreyMartian")
+  found_alien = True
+if antennas <= 6 and eyes >= 2:
+  print("Life form detected: MaxMartian")
+  found_alien = True
+if antennas <= 2 and eyes <= 3:
+  print("Life form detected: BrooklynMartian")
+  found_alien = True
+if antennas == 0 and eyes == 2:
+  print("Life form detected: MattDamonMartian")
+  found_alien = True
+
+#if none of the above conditions are met, output "No life form detected"
+if not found_alien:
+  print("No life form detected")
--- problem2.py
+++ problem2.py
@@ -1,1 +1,24 @@
+"""
+----------------------------------------------------------------------
+Name:      problem2.py
 
+Purpose:   This program will determine if the three side lengths inputted by the user can form a triangle
+ 
+Author:    Guo.T
+ 
+Created:   23/02/2021
+----------------------------------------------------------------------
+"""
+print("******* Traingle Checker ********")
+print()
+
+#take the three side lengths as input
+side1 = int(input("Enter the length of the first side: "))
+side2 = int(input("Enter the length of the second side: "))
+side3 = int(input("Enter the length of the third side: "))
+
+#check if condition is met and output
+if side1 < side2 + side3 and side2 < side1 + side3 and side3 < side1 + side2:
+  print("The figure is a traingle")
+else:
+  print("The figure is NOT a traingle")
